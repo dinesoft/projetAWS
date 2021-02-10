fb36b882960ff62c2d40b1a57fc3726ac178558e
Amandine Thivet <amandinethivet@MacBook-Pro-de-Amandine-2.local>
2021-02-10 11:40:41 +0100

    init

---
+++ secondApp/server.py
@@ -0,0 +1,25 @@
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def dashboard(name):
+    return 'welcome %s' % name
+
+
+@app.route('/index', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['name']
+        print(user)
+        return render_template('index.html')
+    else:
+        user = request.args.get('name')
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=8080)
